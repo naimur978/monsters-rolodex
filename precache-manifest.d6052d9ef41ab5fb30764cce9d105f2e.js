bf3ed50a7d2af43909a573bff4eaa96feb51b220
Naimur Rahman <naimur978@gmail.com>
2020-08-28 22:30:37 +0600

    Updates

---
+++ precache-manifest.d6052d9ef41ab5fb30764cce9d105f2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5422b7b0e9dd0b6af82309a72f65532e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c037475a185f18227243",
+    "url": "/monsters-rolodex/static/css/main.ade1992a.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c037475a185f18227243",
+    "url": "/monsters-rolodex/static/js/main.457ea4f9.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
